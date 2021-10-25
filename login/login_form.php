8800170b5b6e8ddcae6f9a497aec6cd49a50e799
Moobbot <90234479+Moobbot@users.noreply.github.com>
2021-10-25 23:50:12 +0700

    Test2

--- login/login_form.php
+++ login/login_form.php
@@ -9,7 +9,6 @@
         <p class="fw-bold">Password</p> <input type="password" name="" placeholder="Enter Password"> <input
             type="submit" name="" value="Login"> <a href="#">Lost your password?</a><br> <a href="#">Don't have an
             account?</a>
-        hello
     </form>
 </div>
 
