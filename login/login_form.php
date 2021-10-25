8dc65d93eb202c44a2b9e07bec0504fec8042852
duk1321 <duckest100#@gmail.com>
2021-10-25 15:49:50 +0700

    thay doi mot chut cau lenh bootstrap

--- login/login_form.php
+++ login/login_form.php
@@ -4,11 +4,14 @@
 
 <link rel="stylesheet" href="css/style.css">
 
-<div class="loginbox">
-    <h1>Login Here</h1>
+<!--https://bbbootstrap.com/snippets/bootstrap-5-bootstrap5-login-form-91540645-->
+
+
+<div class="loginbox position-absolute">
+    <h1 class="text-center">Login Here</h1>
     <form method="post" action="#">
-        <p>Username</p> <input type="text" name="" placeholder="Enter Username">
-        <p>Password</p> <input type="password" name="" placeholder="Enter Password"> <input type="submit" name="" value="Login"> <a href="#">Lost your password?</a><br> <a href="#">Don't have an account?</a>
+        <p class = "fw-bold">Username</p> <input type="text" name="" placeholder="Enter Username">
+        <p class = "fw-bold">Password</p> <input type="password" name="" placeholder="Enter Password"> <input type="submit" name="" value="Login"> <a href="#">Lost your password?</a><br> <a href="#">Don't have an account?</a>
     </form>
 </div>
 
