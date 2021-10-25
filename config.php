cfe42d61c3251306ebf12d04e6dc5bbe868e9013
Moobbot <90234479+Moobbot@users.noreply.github.com>
2021-10-25 15:10:10 +0700

    Update 0.1

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// Bước 1: Kết nối tới CSDL:
+define('HOST', 'localhost');
+define('USER', 'root');
+const PASS = '';
+const DB = '';
+$conn = mysqli_connect(HOST, USER, PASS, DB);
+if (!$conn) {
+    die('Không thể kết nối');
+}
