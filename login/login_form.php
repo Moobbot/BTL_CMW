c9c8cc1071be4329ce76d3d499b90c0633eeab41
duk1321 <duckest100#@gmail.com>
2021-10-25 15:32:49 +0700

    login form tạm thời

---
+++ login/login_form.php
@@ -0,0 +1,17 @@
+<?php
+    include '../header.php'
+?>
+
+<link rel="stylesheet" href="css/style.css">
+
+<div class="loginbox">
+    <h1>Login Here</h1>
+    <form method="post" action="#">
+        <p>Username</p> <input type="text" name="" placeholder="Enter Username">
+        <p>Password</p> <input type="password" name="" placeholder="Enter Password"> <input type="submit" name="" value="Login"> <a href="#">Lost your password?</a><br> <a href="#">Don't have an account?</a>
+    </form>
+</div>
+
+<?php
+    include '../footer.php'
+?>
