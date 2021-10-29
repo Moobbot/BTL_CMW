ba5742d5538243f9f8974d5a8d2f67a962d2457d
pqduong5401 <phamquangduong542001@gmail.com>
2021-10-29 21:37:18 +0700

    fix loi email

--- signup/process-register.php
+++ signup/process-register.php
@@ -1,55 +1,50 @@
 <?php
 session_start();
 include 'send.php';
-if(isset($_POST['signup'])){
-    $level ;
+if (isset($_POST['signup'])) {
+    $level;
     $user  = $_POST['username'];
     $position = $_POST['position'];
-    if($position == "Giảng Viên")
-    {
+    if ($position == "Giảng Viên") {
         $level = true;
-    }
-    else{
+    } else {
         $level = false;
-
     }
     $email = $_POST['email'];
     $pass1 = $_POST['pass1'];
     $pass2 = $_POST['pass2'];
-    
-}
-   
-
-// Kiểm tra Email hoặc Username đã tồn tại chưa?
-//1. Kết nối tới Server
- include '../reuse/config.php';
-
-// 2. Truy vấn dữ liệu
-$sql = "SELECT * FROM db_users WHERE user_email='$email' OR user_name='$user' ";
-$result = mysqli_query($conn,$sql);
 
 
-// 3. XỬ lý kết quả
-if(mysqli_num_rows($result) > 0){
-    echo 'Wrong';
-    
-}else{
-    // Băm mật khẩu
-    $pass_hash = password_hash($pass1, PASSWORD_DEFAULT);
-    $code = md5(uniqid(rand(), true));
+
+
+    // Kiểm tra Email hoặc Username đã tồn tại chưa?
+    //1. Kết nối tới Server
+    include '../reuse/config.php';
+
+    // 2. Truy vấn dữ liệu
+    $sql = "SELECT * FROM db_users WHERE user_email='$email' OR user_name='$user' ";
+    $result = mysqli_query($conn, $sql);
+
+
+    // 3. XỬ lý kết quả
+    if (mysqli_num_rows($result) > 0) {
+        echo 'Wrong';
+    } else {
+        // Băm mật khẩu
+        $pass_hash = password_hash($pass1, PASSWORD_DEFAULT); // thao tác trên giao diện
+        $code = md5(uniqid(rand(), true));
         //Nếu chưa tồn tại, thì chúng ta mới LƯU vào CSDL và GỬI email xác nhận
 
-    $sql_2 = "INSERT INTO db_users  (user_name,user_email, user_pass,user_level,user_code) VALUES ('$user','$email','$pass_hash','$level','$code');";
+        $sql_2 = "INSERT INTO db_users  (user_name,user_email, user_pass,user_level,user_code) VALUES ('$user','$email','$pass_hash','$level','$code');";
 
-    $result_2 = mysqli_query($conn,$sql_2);
-    if($result_2>0)
-    {
-        echo 'Gửi thành công!';
-        sendEmail($email,$code);
+        $result_2 = mysqli_query($conn, $sql_2);
+       
+        if ($result_2 > 0) {
+            
+            sendEmail($email, $code);
+            echo 'Gửi thành công!';
+        } else {
+            include '../login/index.php';
+        }
     }
-    else{
-        include '../login/index.php';
-    }
-
-
 }
