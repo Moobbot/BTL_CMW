a5e0071343fa8b5ad923483a2281ba7dcc88f5ca
Moobbot <90234479+Moobbot@users.noreply.github.com>
2021-11-03 14:17:14 +0700

    Update footer_body.php

--- reuse/footer_body.php
+++ reuse/footer_body.php
@@ -1,6 +1,17 @@
-<footer class="row w-100 position-absolute bottom-0 p-0 m-0">
-
-    <div class="container-fluid text-center">
-        <h1>Thông tin liên hệ</h1>
+<footer class="row bg-dark position-absolute bottom-0 start-0 end-0 m-0 text-white text-center">
+    <div class="col p-4">
+        <i class="fab fa-github-square"></i>
+        <a href="https://github.com/Moobbot" class="col-3 text-decoration-none text-white">
+            https://github.com/Moobbot</a>
+    </div>
+    <div class="col p-4">
+        <i class="fab fa-github-square"></i>
+        <a href="https://github.com/Duk1321" class="col-3 text-decoration-none text-white">
+            https://github.com/Duk1321</a>
+    </div>
+    <div class="col p-4">
+        <i class="fab fa-github-square"></i>
+        <a href="https://github.com/pqduong5401" class="col-3 text-decoration-none text-white">
+            https://github.com/pqduong5401</a>
     </div>
 </footer>
