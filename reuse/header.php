e17085997451d3259acf8fa2ab5fb7cd3b1e06a8
Moobbot <90234479+Moobbot@users.noreply.github.com>
2021-11-03 14:16:12 +0700

    Update header.php

--- reuse/header.php
+++ reuse/header.php
@@ -10,12 +10,9 @@
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/a4208f5cfb.js" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/CNW/assets/css/bootstrap.min.css">
 
-
-    <link rel="stylesheet" href="./assets/style.css">
-    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="stylesheet" href="http://localhost/CNW/assets/style.css">
 
 </head>
 
