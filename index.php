94f2409f31c91524ed2d8abbf87f8d89491680ab
Moobbot <90234479+Moobbot@users.noreply.github.com>
2021-11-03 14:09:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,45 +1,60 @@
 <?php include 'reuse/header.php' ?>
-<div class="container-fluid ps-0 pe-0">
-    <?php include './reuse/header_body.php' ?>
+<div class="container-fluid px-0">
+    <?php
+    // BEGIN HEADER
 
-    <!-- Body -->
-    <div class="row p-0 m-0 mt-4">
-        <div class="col mb-4">
+    include './reuse/header_body.php';
+
+    // END HEADER
+    ?>
+
+    <?php
+    if (empty($_SESSION['current_user'])) {
+    ?>
+
+    <img src="./assets/img/img_index.jfif" alt="" class="w-100 vh-100">
+
+    <?php
+    } else {
+        // $currentUser = $_SESSION['current_user'];
+    ?>
+
+    <!-- BEGIN CONTAINER -->
+
+    <Section>
+        <div class="row p-0 m-0 mt-4">
             <?php
-
-            // Người dùng chưa đăng nhập
-            if (empty($_SESSION['current_user'])) {
-            ?>
-
-            <h2>Các khóa học hiện tại</h2>
-            <?php include 'subject/index.php'; ?>
-
-            <?php
-            } else {
                 if ($currentUser['user_level'] == 0) {
                     // Hiện thị body admin
-                    include 'admin/index.php';
+                    include './admin/index.php';
+
                 ?>
 
             <?php
                 }
                 if ($currentUser['user_level'] == 1) {
                     // Hiện thị foder teacher
-                    include 'teacher/index.php';
+                    include './teacher/index.php';
                 ?>
-
             <?php
                 }
                 if ($currentUser['user_level'] == 2) {
                     // Hiện thị foder student
                     include './student/index.php';
                 }
-            }
-            ?>
-
+                ?>
         </div>
-    </div>
+    </Section>
+
+    <!-- END CONTAINER -->
+
+    <?php } ?>
+
+    <!-- BEGIN FOOTER -->
 
     <?php include './reuse/footer_body.php' ?>
+
+    <!-- END FOOTER -->
+
 </div>
 <?php include 'reuse/footer.php' ?>
