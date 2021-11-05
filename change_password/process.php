654875056d63b8b4ae54a181323cef6fbcea985c
duk1321 <duckest100#@gmail.com>
2021-11-05 12:03:09 +0700

    thêm sự kiện kiểm tra rỗng mật khẩu

--- change_password/process.php
+++ change_password/process.php
@@ -10,24 +10,32 @@
     }else{
         $pass = $_POST['txtPass'];
         $pass1 = $_POST['txtPass1'];
-        if($pass == $pass1){
-            $password_hash = password_hash($pass,PASSWORD_DEFAULT);
-            $user = $_SESSION['current_user'];
-            $username = $user['user_name'];
-            $sql = "UPDATE db_users SET user_pass = '$password_hash' WHERE user_name = '$username'";
-            $result = mysqli_query($conn,$sql);
-            unset($_SESSION['current_user']);
+        if($pass == '' || $pass1 == ''){
             echo json_encode(array(
-                'status' => 1,
-                'message' => 'Thay đổi thành công, hãy đăng nhập bằng tài khoản mới'
+                'status' => 0,
+                'message' => 'Hãy nhập mật khẩu'
             ));
             exit;
         }else{
-            echo json_encode(array(
-                'status' => 0,
-                'message' => 'Mật khẩu nhập lại không chính xác'
-            ));
-            exit;
+            if($pass == $pass1){
+                $password_hash = password_hash($pass,PASSWORD_DEFAULT);
+                $user = $_SESSION['current_user'];
+                $username = $user['user_name'];
+                $sql = "UPDATE db_users SET user_pass = '$password_hash' WHERE user_name = '$username'";
+                $result = mysqli_query($conn,$sql);
+                unset($_SESSION['current_user']);
+                echo json_encode(array(
+                    'status' => 1,
+                    'message' => 'Thay đổi thành công, hãy đăng nhập bằng tài khoản mới'
+                ));
+                exit;
+            }else{
+                echo json_encode(array(
+                    'status' => 0,
+                    'message' => 'Mật khẩu nhập lại không chính xác'
+                ));
+                exit;
+            }
         }
     }
 ?>
